0cc7aaaee675017eda59fc01da729a973843cefc
Mikhail Zolotukhin <mail@gikari.com>
2022-11-26 01:37:31 +0100

    refactor: add clang-format and apply it

--- src/numbers_column.cpp
+++ src/numbers_column.cpp
@@ -49,34 +49,36 @@
 #include <texteditor/texteditor.h>
 #include <texteditor/texteditorsettings.h>
 
-#include <QScrollBar>
 #include <QPainter>
+#include <QScrollBar>
 
 namespace QNVim {
 namespace Internal {
 
-NumbersColumn::NumbersColumn()
-{
+NumbersColumn::NumbersColumn() {
     setAttribute(Qt::WA_TransparentForMouseEvents, true);
     connect(TextEditor::TextEditorSettings::instance(),
-        &TextEditor::TextEditorSettings::displaySettingsChanged,
-        this, &NumbersColumn::updateGeometry);
+            &TextEditor::TextEditorSettings::displaySettingsChanged,
+            this, &NumbersColumn::updateGeometry);
 }
 
 void NumbersColumn::setEditor(TextEditor::TextEditorWidget *editor) {
     if (editor == mEditor)
         return;
+
     if (mEditor) {
         mEditor->removeEventFilter(this);
         disconnect(mEditor, &QPlainTextEdit::cursorPositionChanged,
-                this, &NumbersColumn::updateGeometry);
+                   this, &NumbersColumn::updateGeometry);
         disconnect(mEditor->verticalScrollBar(), &QScrollBar::valueChanged,
-                this, &NumbersColumn::updateGeometry);
+                   this, &NumbersColumn::updateGeometry);
         disconnect(mEditor->document(), &QTextDocument::contentsChanged,
-                this, &NumbersColumn::updateGeometry);
+                   this, &NumbersColumn::updateGeometry);
     }
+
     mEditor = editor;
     setParent(mEditor);
+
     if (mEditor) {
         mEditor->installEventFilter(this);
         connect(mEditor, &QPlainTextEdit::cursorPositionChanged,
@@ -86,9 +88,9 @@ void NumbersColumn::setEditor(TextEditor::TextEditorWidget *editor) {
         connect(mEditor->document(), &QTextDocument::contentsChanged,
                 this, &NumbersColumn::updateGeometry);
         show();
-    }
-    else
+    } else
         hide();
+
     updateGeometry();
 }
 
@@ -100,8 +102,10 @@ void NumbersColumn::setNumber(bool number) {
 void NumbersColumn::paintEvent(QPaintEvent *event) {
     if (not mEditor)
         return;
+
     QTextCursor firstVisibleCursor = mEditor->cursorForPosition(QPoint(0, 0));
     QTextBlock firstVisibleBlock = firstVisibleCursor.block();
+
     if (firstVisibleCursor.positionInBlock() > 0) {
         firstVisibleBlock = firstVisibleBlock.next();
         firstVisibleCursor.setPosition(firstVisibleBlock.position());
@@ -110,8 +114,10 @@ void NumbersColumn::paintEvent(QPaintEvent *event) {
     QTextBlock block = mEditor->textCursor().block();
     bool forward = firstVisibleBlock.blockNumber() > block.blockNumber();
     int n = 0;
+
     while (block.isValid() and block != firstVisibleBlock) {
         block = forward ? block.next() : block.previous();
+
         if (block.isVisible())
             n += forward ? 1 : -1;
     }
@@ -121,15 +127,18 @@ void NumbersColumn::paintEvent(QPaintEvent *event) {
     const QColor fg = pal.color(QPalette::Dark);
     const QColor bg = pal.color(QPalette::Background);
     p.setPen(fg);
+
     QFontMetricsF fm(mEditor->textDocument()->fontSettings().font());
     qreal lineHeight = block.layout()->boundingRect().height();
     QRectF rect(0, mEditor->cursorRect(firstVisibleCursor).y(), width(), lineHeight);
     bool hideLineNumbers = mEditor->lineNumbersVisible();
+
     while (block.isValid()) {
         if (block.isVisible()) {
             if ((not mNumber or n != 0) and rect.intersects(event->rect())) {
                 const int line = qAbs(n);
                 const QString number = QString::number(line);
+
                 if (hideLineNumbers)
                     p.fillRect(rect, bg);
                 if (hideLineNumbers or line < 100)
@@ -150,12 +159,14 @@ void NumbersColumn::paintEvent(QPaintEvent *event) {
 bool NumbersColumn::eventFilter(QObject *, QEvent *event) {
     if (event->type() == QEvent::Resize or event->type() == QEvent::Move)
         updateGeometry();
+
     return false;
 }
 
 void NumbersColumn::updateGeometry() {
     if (not mEditor)
         return;
+
     QFontMetrics fm(mEditor->textDocument()->fontSettings().font());
     int lineHeight = fm.lineSpacing();
     setFont(mEditor->extraArea()->font());
@@ -164,10 +175,13 @@ void NumbersColumn::updateGeometry() {
     bool marksVisible = mEditor->marksVisible();
     bool lineNumbersVisible = mEditor->lineNumbersVisible();
     bool foldMarksVisible = mEditor->codeFoldingVisible();
+
     if (marksVisible and lineNumbersVisible)
         rect.setLeft(lineHeight);
+
     if (foldMarksVisible and (marksVisible or lineNumbersVisible))
         rect.setRight(rect.right() - (lineHeight + lineHeight % 2));
+
     setGeometry(rect);
 
     update();
